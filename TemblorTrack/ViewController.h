178f98b1972119c46f915d8b41861a7852a376dd
Scott Ferwerda <srf@ip-10-0-30-18.ec2.internal>
2015-07-28 20:20:03 -0400

    Initial Commit

---
+++ TemblorTrack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TemblorTrack
+//
+//  Created by Scott Ferwerda on 7/28/15.
+//  Copyright (c) 2015 Scott Ferwerda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TemblorTrack/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TemblorTrack
+//
+//  Created by Scott Ferwerda on 7/28/15.
+//  Copyright (c) 2015 Scott Ferwerda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
